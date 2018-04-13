3685e4830aa23709238f34fb60472003d8be1dbe
Tam <luongtam1101995>
2018-04-13 17:47:20 +0700

    non

---
+++ Helper/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Maxx
+ * @category    Maxx
+ * @package     Maxx_AgePopup
+ * @copyright   Copyright (c) 2018 Maxx
+ */
+
+namespace Mageplaza\Smtp\Helper;
+
+use Mageplaza\Core\Helper\AbstractData;
+
+/**
+ * Class Data
+ * @package Mageplaza\Smtp\Helper
+ */
+class Config extends AbstractData
+{
+
+}
---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Maxx
+ * @category    Maxx
+ * @package     Maxx_AgePopup
+ * @copyright   Copyright (c) 2018 Maxx
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Maxx_AgePopup',
+    __DIR__
+);
